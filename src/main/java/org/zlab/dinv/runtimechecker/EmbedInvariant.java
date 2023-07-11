c9730ec7bb0a44fc91eb0676933cea2df4c4e4e4
Ke Han <kehan5800@gmail.com>
2023-07-11 10:03:10 -0400

    cli for invariants embedding

---
+++ src/main/java/org/zlab/dinv/runtimechecker/EmbedInvariant.java
@@ -0,0 +1,71 @@
+package org.zlab.dinv.runtimechecker;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import org.zlab.dinv.Config;
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+
+@CommandLine.Command(name = "EmbedInvariant", mixinStandardHelpOptions = true, version = "1.0",
+        description = "EmbedInvariant does amazing things.")
+public class EmbedInvariant implements Runnable {
+
+    @CommandLine.Option(names = { "-infoPath" }, description = "path to files generated from vasco")
+    private String infopath;
+
+    @CommandLine.Option(names = { "-targetSystemPath" }, description = "path to system being rewritten")
+    private String targetSystemPath;
+
+    @Override
+    public void run() {
+        Path systemInfoPath = Config.systemInfoPath;
+        if (infopath != null) {
+            systemInfoPath = Paths.get(infopath);
+        }
+        Path projectRootDir = Config.projectRootDir;
+        if (targetSystemPath != null) {
+            projectRootDir = Paths.get(targetSystemPath);
+        }
+        System.out.println("[RewriteExec] systemInfoPath = " + systemInfoPath);
+        System.out.println("[RewriteExec] target projectRootDir = " + projectRootDir);
+
+        Path targetInvPath = systemInfoPath.resolve("inv.txt");
+        Path targetIsSerializeInvPath = systemInfoPath.resolve("isSerializeInvs.txt");
+
+        Map<String, Set<String>> invs =  LoadInvariant.load(targetInvPath);
+        if (targetIsSerializeInvPath.toFile().exists()) {
+            Map<String, Set<String>> isSerializeInvs =  LoadInvariant.load(targetIsSerializeInvPath);
+            Utils.mergeInv(invs, isSerializeInvs);
+        } else {
+            System.out.println("[Warning] isSerialized inv is not provided");
+        }
+
+        // Walk the project directory structure and find all the Java source files
+        try {
+            Files.walk(projectRootDir)
+                    .filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".java"))
+                    .forEach(p -> {
+                        try {
+                            // debug
+                            // if (!p.toString().contains("DataLimits")) return;
+                            CompilationUnit cu = StaticJavaParser.parse(p.toFile());
+                            // Traverse the AST and perform the desired processing
+                            cu.accept(new InstrumentInvariant.InstClassVisitor(invs), null);
+
+                            Files.write(p, cu.toString().getBytes());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/org/zlab/dinv/runtimechecker/Main.java
+++ src/main/java/org/zlab/dinv/runtimechecker/Main.java
@@ -1,75 +1,15 @@
 package org.zlab.dinv.runtimechecker;
 
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.rmi.RemoteException;
-import java.util.Map;
-import java.util.Set;
+import picocli.CommandLine;
 
 public class Main {
-    // input: inv, source code path
-    // output: embed the invariants into the program points
 
-    // process the entire project
-    public static String cassandrRootDir = "/Users/hanke/Project/cassandra/cassandra1/src/java/org/apache/cassandra";
-    public static String cassInvPath = "/Users/hanke/Desktop/Project/vasco/output/system/cassandra/apache-cassandra-3.11.15/inv.txt";
-    public static String cassIsSerializeInvPath = "/Users/hanke/Desktop/Project/vasco/output/system/cassandra/apache-cassandra-3.11.15/isSerializeInvs.txt";
-
-    public static String hdfsRootDir = "/Users/hanke/Desktop/Project/hadoop/hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server";
-    public static String hdfsInvPath = "input/target_inv_hdfs_jml";
-    public static String hdfsIsSerializeInvPath = "/Users/hanke/Desktop/Project/vasco/output/system/cassandra/apache-cassandra-3.11.15/isSerializeInvs.txt";
-
-    public static String targetSystem = "cassandra";
-
-    public static void test() throws IOException {
-        String projectRootDir;
-        Path targetInv;
-        Path targetIsSerializeInv;
-
-        if (targetSystem.equals("cassandra")) {
-            projectRootDir = cassandrRootDir;
-            targetInv = Paths.get(cassInvPath);
-            targetIsSerializeInv = Paths.get(cassIsSerializeInvPath);
-        } else if (targetSystem.equals("hdfs")) {
-            projectRootDir = hdfsRootDir;
-            targetInv = Paths.get(hdfsInvPath);
-            targetIsSerializeInv = Paths.get(hdfsIsSerializeInvPath);
-        } else {
-            throw new RemoteException("only tested on cassandra or hdfs");
-        }
-
-        Map<String, Set<String>> invs =  LoadInvariant.load(targetInv);
-        Map<String, Set<String>> isSerializeInvs =  LoadInvariant.load(targetIsSerializeInv);
-        Utils.mergeInv(invs, isSerializeInvs);
-
-        // Walk the project directory structure and find all the Java source files
-        Files.walk(Paths.get(projectRootDir))
-                .filter(Files::isRegularFile)
-                .filter(p -> p.toString().endsWith(".java"))
-                .forEach(p -> {
-                    try {
-                        // debug
-                        // if (!p.toString().contains("DataLimits")) return;
-                        CompilationUnit cu = StaticJavaParser.parse(p.toFile());
-                        // Traverse the AST and perform the desired processing
-                        cu.accept(new InstrumentInvariant.InstClassVisitor(invs), null);
-
-                        Files.write(p, cu.toString().getBytes());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-    }
-
-    public static void main(String[] args) throws IOException {
-        test();
+    public static void main(String[] args) {
+        EmbedInvariant embedInvariant = new EmbedInvariant();
+        new CommandLine((embedInvariant)).execute(args);
     }
 
     public static void rewriteFieldsAsPublic(CompilationUnit cu) {
