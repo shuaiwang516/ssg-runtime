a78b3813238c7e8b26b1e6c7890b83e7f397bd24
Ke Han <kehan5800@gmail.com>
2023-07-23 14:30:45 -0400

    avoid including configurations from inner classes

--- src/main/java/org/zlab/dinv/diffconfig/ConfigVisitor.java
+++ src/main/java/org/zlab/dinv/diffconfig/ConfigVisitor.java
@@ -1,5 +1,6 @@
 package org.zlab.dinv.diffconfig;
 
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.type.Type;
@@ -8,29 +9,35 @@ import com.github.javaparser.resolution.UnsolvedSymbolException;
 import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import com.github.javaparser.resolution.types.ResolvedType;
 
+import java.util.List;
+
 public class ConfigVisitor extends VoidVisitorAdapter<SingleClassConfigInfo> {
 
     @Override
-    public void visit(FieldDeclaration field, SingleClassConfigInfo singleClassConfigInfo) {
-        super.visit(field, singleClassConfigInfo);
-        for (VariableDeclarator variableDeclarator: field.getVariables()) {
-            // we might resolve it
-            Type type = variableDeclarator.getType();
-            String typeString = type.asString();
-            try {
-                ResolvedType resolvedType = type.resolve();
-                if (resolvedType.isReferenceType()) {
-                    ResolvedReferenceType resolvedReferenceType = resolvedType.asReferenceType();
-                    typeString = resolvedReferenceType.getQualifiedName();
+    public void visit(ClassOrInterfaceDeclaration classOrInterfaceDeclaration, SingleClassConfigInfo singleClassConfigInfo) {
+        // there's no need to iterate the inner static class since it's done outside
+        // super.visit(classOrInterfaceDeclaration, singleClassConfigInfo);
+
+        List<FieldDeclaration> fields = classOrInterfaceDeclaration.getFields();
+        for (FieldDeclaration field: fields) {
+            for (VariableDeclarator variableDeclarator: field.getVariables()) {
+                // we might resolve it
+                Type type = variableDeclarator.getType();
+                String typeString = type.asString();
+                try {
+                    ResolvedType resolvedType = type.resolve();
+                    if (resolvedType.isReferenceType()) {
+                        ResolvedReferenceType resolvedReferenceType = resolvedType.asReferenceType();
+                        typeString = resolvedReferenceType.getQualifiedName();
+                    }
+                } catch (UnsolvedSymbolException ignored) {}
+
+                singleClassConfigInfo.typeCollector.put(variableDeclarator.getNameAsString(), typeString);
+
+                if (variableDeclarator.getInitializer().isPresent()) {
+                    singleClassConfigInfo.initCollector.put(variableDeclarator.getNameAsString(), variableDeclarator.getInitializer().get().toString());
                 }
-            } catch (UnsolvedSymbolException ignored) {}
-
-            singleClassConfigInfo.typeCollector.put(variableDeclarator.getNameAsString(), typeString);
-
-            if (variableDeclarator.getInitializer().isPresent()) {
-                singleClassConfigInfo.initCollector.put(variableDeclarator.getNameAsString(), variableDeclarator.getInitializer().get().toString());
             }
         }
     }
-
 }
