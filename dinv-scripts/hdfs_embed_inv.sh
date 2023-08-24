1495ae6372dec67c16cf7586e35612eb6f8a2a65
Ke Han <kehan5800@gmail.com>
2023-08-24 16:14:01 -0400

    update script

--- dinv-scripts/hdfs_embed_inv.sh
+++ dinv-scripts/hdfs_embed_inv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SYSTEM_PATH1="/Users/hanke/Desktop/Project/hadoop/hadoop1/hadoop-common-project"
-SYSTEM_PATH2="/Users/hanke/Desktop/Project/hadoop/hadoop1/hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/"
+SYSTEM_PATH1="/Users/hanke/Desktop/Project/hadoop/hadoop1/hadoop-common-project"
+SYSTEM_PATH2="/Users/hanke/Desktop/Project/hadoop/hadoop1/hadoop-hdfs-project"
+
 INFO_PATH="/Users/hanke/Desktop/Project/vasco/system/hdfs/hadoop-3.3.0"
 
-./gradlew embedInv --args="-infoPath ${INFO_PATH} -targetSystemPath ${SYSTEM_PATH2}"
-
+./gradlew embedInv --args="-infoPath ${INFO_PATH} -targetSystemPath ${SYSTEM_PATH1} -targetSystemPath ${SYSTEM_PATH2}"
