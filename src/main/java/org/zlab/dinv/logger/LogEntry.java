9e8cce11ac78754eb355c41c1df5279aa37e4fba
Ke Han <kehan5800@gmail.com>
2023-11-17 23:41:57 -0500

    json format

---
+++ src/main/java/org/zlab/dinv/logger/LogEntry.java
@@ -0,0 +1,116 @@
+package org.zlab.dinv.logger;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LogEntry {
+    public int threadId;
+    public VariableInfo object;
+    public VariableInfo field;
+
+    public LogEntry() {
+    }
+
+    public LogEntry(int threadId, Object object, Object field, String name) {
+        this.threadId = threadId;
+        this.object = createVariableInfo(object, null);
+        this.field = createVariableInfo(field, name);
+    }
+
+    public enum VariableType {
+        INTEGER, FLOAT, BOOLEAN, CHARACTER, BYTE, SHORT, LONG, DOUBLE, STRING, ENUM, UNKNOWN;
+
+        public static VariableType getVariableType(Object object) {
+            if (object instanceof Integer) {
+                return VariableType.INTEGER;
+            } else if (object instanceof Float) {
+                return VariableType.FLOAT;
+            } else if (object instanceof Boolean) {
+                return VariableType.BOOLEAN;
+            } else if (object instanceof Character) {
+                return VariableType.CHARACTER;
+            } else if (object instanceof Byte) {
+                return VariableType.BYTE;
+            } else if (object instanceof Short) {
+                return VariableType.SHORT;
+            } else if (object instanceof Long) {
+                return VariableType.LONG;
+            } else if (object instanceof Double) {
+                return VariableType.DOUBLE;
+            } else if (object instanceof String) {
+                return VariableType.STRING;
+            } else if (object instanceof Enum) {
+                return VariableType.ENUM;
+            } else {
+                return VariableType.UNKNOWN;
+            }
+        }
+
+        public boolean isPrimitive() {
+            return this != STRING && this != ENUM && this != UNKNOWN;
+        }
+    }
+
+    public static class VariableInfo {
+        public VariableType type; // This will hold either the primitive type name or "String"
+        public String className;
+        public int identifyHash = 0;
+        public String value; // hashcode for object, value for primitive
+        public String name = null; // field name
+        public VariableInfo() {
+        }
+        @Override
+        public String toString() {
+            return "VariableInfo{" + "type=" + type + ", className='" + className + '\''
+                    + ", identifyHash=" + identifyHash + ", value='" + value + '\'' + ", name='"
+                    + name + '\'' + '}';
+        }
+    }
+
+    public static VariableInfo createVariableInfo(Object var, String name) {
+        // Add a checker: if it's primitive, we don't use identified hashcode
+        VariableInfo varInfo = new VariableInfo();
+        // type
+        varInfo.type = VariableType.getVariableType(var);
+        // className
+        varInfo.className = var.getClass().getName();
+        // identifyHash
+        if (!varInfo.type.isPrimitive())
+            varInfo.identifyHash = System.identityHashCode(var);
+        // value
+        varInfo.value = var.toString();
+        // name
+        varInfo.name = name;
+        return varInfo;
+    }
+
+    // transform to json string
+    public String toJsonString() {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString;
+        try {
+            jsonString = mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return jsonString;
+    }
+
+    public static LogEntry fromJsonString(String jsonString) {
+        ObjectMapper mapper = new ObjectMapper();
+        LogEntry logEntry;
+        try {
+            logEntry = mapper.readValue(jsonString, LogEntry.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return logEntry;
+    }
+
+    @Override
+    public String toString() {
+        return "LogEntry{" + "threadId=" + threadId + ", object=" + object + ", field=" + field
+                + '}';
+    }
+
+}
---
+++ src/test/java/org/zlab/dinv/logger/TestLogger.java
@@ -0,0 +1,72 @@
+package org.zlab.dinv.logger;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestLogger {
+    @Test
+    public void test() {
+        int a = 0;
+        int b = System.identityHashCode(a);
+        System.out.println(b);
+    }
+
+    @Test
+    public void testType() {
+        Tmp t = new Tmp();
+        int a = t.a;
+        String name = "a";
+        LogEntry logEntry = new LogEntry(0, t, a, name);
+        String s1 = logEntry.toString();
+        String json = logEntry.toJsonString();
+
+        // open a file /tmp/log.json and write json to it
+        String filename = "/tmp/testLog";
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        LogEntry newLogEntry = LogEntry.fromJsonString(json);
+        String s2 = newLogEntry.toString();
+        System.out.println(s1);
+        System.out.println(s2);
+
+        assert s1.equals(s2);
+
+        String json3 = readFirstLineFromFile(filename);
+        LogEntry newLogEntry2 = LogEntry.fromJsonString(json3);
+        String s3 = newLogEntry2.toString();
+        System.out.println(s3);
+        assert s1.equals(s3);
+
+    }
+
+    @Test
+    public void testReadLog() {
+    }
+
+    public static class Tmp {
+        public int a = 0;
+    }
+
+    public static void foo(Object a) {
+
+    }
+
+    public static String readFirstLineFromFile(String filename) {
+        String line = null;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            line = reader.readLine(); // Reads the first line of the file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+}
