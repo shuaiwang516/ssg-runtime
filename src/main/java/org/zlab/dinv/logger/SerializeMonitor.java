bf103033ad22be79246e8135bce409191fcbe7e7
Ke Han <kehan5800@gmail.com>
2023-11-27 17:15:55 -0500

    add comment

--- src/main/java/org/zlab/dinv/logger/SerializeMonitor.java
+++ src/main/java/org/zlab/dinv/logger/SerializeMonitor.java
@@ -1,5 +1,6 @@
 package org.zlab.dinv.logger;
 
 public class SerializeMonitor {
+    // org.zlab.dinv.logger.SerializeMonitor.isSerializing = true;
     public static boolean isSerializing = false;
 }
