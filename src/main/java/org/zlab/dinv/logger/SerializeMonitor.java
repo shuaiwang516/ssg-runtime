ef48efdb0c98463d7ffc35f5f3749add8f30f209
Ke Han <kehan5800@gmail.com>
2025-01-01 17:02:23 -0500

    deprecate dinv related code

--- src/main/java/org/zlab/dinv/logger/SerializeMonitor.java
+++
@@ -1,6 +0,0 @@
-package org.zlab.dinv.logger;
-
-public class SerializeMonitor {
-    // org.zlab.dinv.logger.SerializeMonitor.isSerializing = true;
-    public static boolean isSerializing = false;
-}
