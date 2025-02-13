08d173b4e2f22b20a1d7097a60d08a9c64de294e
Ke Han <kehan5800@gmail.com>
2025-02-13 01:53:11 -0500

    fix bug: init problem

--- src/main/java/org/zlab/net/tracker/Trace.java
+++ src/main/java/org/zlab/net/tracker/Trace.java
@@ -1,10 +1,11 @@
 package org.zlab.net.tracker;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Trace implements Serializable {
-    List<TraceEntry> traceEntries;
+    List<TraceEntry> traceEntries = new LinkedList<>();
 
     public void record(String name, int id, Object... contextArgs) {
         traceEntries.add(new TraceEntry(id));
