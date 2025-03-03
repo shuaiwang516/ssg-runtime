08ac786a3656dd3e7bafe612562aef18cf697e81
Ke Han <kehan5800@gmail.com>
2025-03-02 22:29:27 -0600

    diff compute implementation

--- src/main/java/org/zlab/net/tracker/DiffComputeEditDistance.java
+++ src/main/java/org/zlab/net/tracker/DiffComputeEditDistance.java
@@ -5,12 +5,13 @@ import org.apache.commons.lang3.NotImplementedException;
 public class DiffComputeEditDistance {
     public static int compute(Trace trace0, Trace trace1) {
         // diff 0 and 1
+        // How to compute? suppose for a sequential list with multiple nodes...
         throw new NotImplementedException("Not implemented yet");
     }
 
-    public static int compute(Trace trace0, Trace trace1, Trace trace2) {
+    public static int[] compute(Trace trace0, Trace trace1, Trace trace2) {
         // diff 0 and 1
         // diff 1 and 2
-        throw new NotImplementedException("Not implemented yet");
+        return new int[]{compute(trace0, trace1), compute(trace1, trace2)};
     }
 }
