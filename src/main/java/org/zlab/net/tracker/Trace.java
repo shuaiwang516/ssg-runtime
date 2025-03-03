93d965e0cac888505125cf73534626adf572fed9
Ke Han <kehan5800@gmail.com>
2025-03-03 14:38:23 -0600

    add assertion

--- src/main/java/org/zlab/net/tracker/Trace.java
+++ src/main/java/org/zlab/net/tracker/Trace.java
@@ -17,6 +17,9 @@ public class Trace implements Serializable {
     public void append(Trace trace) {
         if (trace == null)
             return;
+        assert traceEntries.isEmpty() || traceEntries
+                .get(traceEntries.size() - 1).timestamp < trace.traceEntries.get(0).timestamp
+                : "The trace to be appended is not in the correct order";
         traceEntries.addAll(trace.traceEntries);
     }
 
